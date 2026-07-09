da21517f713c59d94e6cf628a3cdb2be86074769
Joao <joao.v.arbegaus@gmail.com>
2026-07-09 15:22:18 -0300

    implementa client feign

--- src/main/java/com/wakanda/wakacop/associado/application/service/AssociadoApplicationService.java
+++ src/main/java/com/wakanda/wakacop/associado/application/service/AssociadoApplicationService.java
@@ -1,5 +1,7 @@
 package com.wakanda.wakacop.associado.application.service;
 
+import com.wakanda.wakacop.associado.infra.client.ConsultaCPFResponse;
+import com.wakanda.wakacop.associado.infra.client.SerproClientFeign;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
@@ -8,13 +10,24 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Log4j2
 public class AssociadoApplicationService implements AssociadoService {
+    private final SerproClientFeign serproClientFeign;
+
+    private static final String TOKEN = "Bearer 06aef429-a981-3ec5-a1f8-71d38d86481e";
+
     @Override
     public void validaAssociadoAptoVoto(String cpfAssociado) {
         log.info("[start] AssociadoApplicationService - validaAssociadoAptoVoto");
 
-
-
+        ConsultaCPFResponse consultaCPFResponse = serproClientFeign.consultaCpf(TOKEN, cpfAssociado);
+        valida(consultaCPFResponse);
 
         log.info("[finish] AssociadoApplicationService - validaAssociadoAptoVoto");
     }
+
+    private void valida(ConsultaCPFResponse consultaCPFResponse) {
+        if(consultaCPFResponse.isInvalid()){
+            throw new RuntimeException("CPF associado inválido");
+        }
+    }
+
 }
